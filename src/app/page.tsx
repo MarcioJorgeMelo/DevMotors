a4e600c4b1eb2efd3d69e7683ebde1a8a6125888
Marcio Jorge Medeiros Melo Filho <marcio.melo@livsaude.com.br>
2025-01-27 11:45:55 -0300

    feat: add footer-component on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import { Hero } from "./components/hero";
 import { Phone } from "lucide-react";
 import { Services } from "./components/home/services";
 import { Container } from "./components/container";
+import { Footer } from "./components/home/footer";
 
 
 export default async function Home() {
@@ -23,6 +24,8 @@ export default async function Home() {
 
       <Container>
         <Services object={object} />
+
+        <Footer object={object} />
       </Container>
     </main>
   );
