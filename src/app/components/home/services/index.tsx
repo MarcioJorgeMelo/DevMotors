34633cd167714ed832c112b44344a5bc5444244e
Marcio Jorge Medeiros Melo Filho <marcio.melo@livsaude.com.br>
2025-01-27 10:23:37 -0300

    feat: add sobre on services component

--- src/app/components/home/services/index.tsx
+++ src/app/components/home/services/index.tsx
@@ -4,21 +4,42 @@ import Image from "next/image";
 
 export function Services({ object }: HomeProps) {
     return (
-        <section className={styles.containerAbout}>
-            <article className={styles.innerAbout}>
-                <h1 className={styles.title}>Sobre</h1>
-                <p>{object.metadata.about.description}</p>
-            </article>
+        <>
+            <section className={styles.containerAbout} id="servicos">
+                <article className={styles.innerAbout}>
+                    <h1 className={styles.title}>Sobre</h1>
+                    <p>{object.metadata.about.description}</p>
+                </article>
 
-            <div className={styles.bannerAbout}>
-                <Image 
-                    className={styles.imageAbout}
-                    alt="Imagem ilustrativa sobre a empresa"
-                    quality={100}
-                    fill={true}
-                    src={object.metadata.about.banner.url}
-                />
-            </div>
-        </section>
+                <div className={styles.bannerAbout}>
+                    <Image 
+                        className={styles.imageAbout}
+                        alt="Imagem ilustrativa sobre a empresa"
+                        quality={100}
+                        fill={true}
+                        src={object.metadata.about.banner.url}
+                    />
+                </div>
+            </section>
+
+            <h2 className={styles.servicesTitle}>Conheça nossos serviços</h2>
+
+            <section className={styles.services}>
+                {object.metadata.services.map((item) => (
+                    <article key={item.description} className={styles.service}>
+                        <div className={styles.innerService}>
+                            <Image
+                                className={styles.imageService}
+                                alt="Imagem do serviço"
+                                quality={100}
+                                fill={true}
+                                src={item.image.url}
+                            />
+                        </div>
+                        <p>{item.description}</p>
+                    </article>
+                ))}
+            </section>
+        </>
     )
 }
