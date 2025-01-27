bfbf40197ffbc434f15ac228cb5807c8abc5316d
Marcio Jorge Medeiros Melo Filho <marcio.melo@livsaude.com.br>
2025-01-27 09:21:34 -0300

    feat: add hero component on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import { HomeProps } from "@/utils/home.type";
 import { Submenu } from "./components/home/submenu";
 import { getDataHome } from "@/utils/actions/get-data";
+import { Hero } from "./components/hero";
+import { Phone } from "lucide-react";
 
 
 export default async function Home() {
@@ -9,6 +11,13 @@ export default async function Home() {
   return (
     <main>
       <Submenu />
+      <Hero
+        heading={data.object.metadata.heading}
+        buttonTitle={data.object.metadata.cta_button.title}
+        buttonUrl={data.object.metadata.cta_button.url}
+        bannerUrl={data.object.metadata.banner.url}
+        icon={<Phone size={24} color="#FFF" />}
+      />
     </main>
   );
 }
